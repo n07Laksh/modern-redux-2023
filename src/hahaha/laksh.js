adc06a4d5cd6a55a76a6aaf535717bea49d8d70f
n07Laksh <112006645+n07Laksh@users.noreply.github.com>
2023-07-03 00:46:43 +0530

    new console

--- src/hahaha/laksh.js
+++ src/hahaha/laksh.js
@@ -9,14 +9,14 @@ console.log("hello")
 
 
 
-console.log("hello")
-console.log("hello")
+console.log("hello hahahahahahahhahahahahahahah")
+console.log("hello hahahahahahahhahahahahahahah")
 
 
-console.log("hello")
+console.log("hello hahahahahahahhahahahahahahah")
 
-console.log("hello")
+console.log("hello hahahahahahahhahahahahahahah")
 
-console.log("hello")
+console.log("hello hahahahahahahhahahahahahahah")
 
-console.log("hello")
+console.log("hello hahahahahahahhahahahahahahah")
